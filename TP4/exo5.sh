7bd8ecf1d469485b1e357587a8ef024da6e98fcd
amoussa.rayan77130@hotmail.fr <amoussa.rayan77130@hotmail.fr>
2024-10-21 19:38:44 +0200

    maj

--- TP4/exo5.sh
+++ TP4/exo5.sh
@@ -1,2 +1,23 @@
 #!/bin/bash
 
+if [ $# -eq 0 ];
+ then
+    echo "Usage: $0 <répertoire> <répertoire> ... "
+    exit 1
+fi
+
+failed_finds=0
+index=1
+
+for arg in "$@";
+do
+
+    find $arg -type d 2> find_results/find-$index.log > find_results/find-$index.txt    
+
+    if [ $? -ne 0 ]
+        then ((failed_finds++))
+    fi    
+    ((index++))
+done
+
+exit $failed_finds
